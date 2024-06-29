a55433940c5ceb74ba1ee9f7b7f3a3031ce8e7ee
yukihito <ok220020@oks.iput.ac.jp>
2024-06-29 20:04:16 +0900

    ハイパーパラメータ表示

--- dev/frontend/reactapp/src/components/ModelCreateTrain/EditTileParameterField.js
+++ dev/frontend/reactapp/src/components/ModelCreateTrain/EditTileParameterField.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import './ModelCreateTrain.css';
 import EditTileParamet from './EditTileParamet';
+import { v4 as uuidv4 } from 'uuid';
 
 function EditTileParameterField({ parameter }) {
+  const pList =["kernel_size", "activ_func", "out_channel", "padding", "strid", "dropout_p", "input_size"]
   const [keys, setKeys] = useState([]);
   useEffect(() => {
     const handleP = () => {
       if (parameter !== null) {
         const keys = Object.keys(parameter);
-        setKeys(keys);
+        const sortedKeys = keys.sort((a, b) => a.localeCompare(b));
+        setKeys(sortedKeys);
       };
     };
     handleP();
@@ -18,9 +21,11 @@ function EditTileParameterField({ parameter }) {
       <div className='edit-tile-field'>
         
         {parameter && keys.map((key, index) => (
-          <div key={index}>
-            <EditTileParamet name={key} value={parameter[key]} />
-          </div>
+          pList.includes(String(key)) && (
+            <div key={index}>
+              <EditTileParamet name={key} value={parameter[key]} />
+            </div>
+          )
         ))}
       </div>
     </div>
