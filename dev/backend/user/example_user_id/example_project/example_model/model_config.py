660b0230189ab6b4c726d830f5f6972712861f13
riku1021 <iriku63@gmail.com>
2024-07-05 16:39:30 +0900

    CNNのpyファイル作成プログラム作成

---
+++ dev/backend/user/example_user_id/example_project/example_model/model_config.py
@@ -0,0 +1,24 @@
+
+import torch.nn as nn
+
+class Simple_NN(nn.Module):
+    def __init__(self):
+        super(Simple_NN, self).__init__()
+
+        self.seq = nn.Sequential(
+            nn.Conv2d(1, 64, 3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.BatchNorm2d(64),
+            nn.AdaptiveMaxPool2d(1),
+            nn.Flatten(),
+            nn.Linear(100, 100),
+            nn.Tanh(),
+            nn.Linear(100, 100),
+            nn.Sigmoid(),
+            nn.Linear(100, 10),
+
+            )
+
+    def forward(self, x):
+        return self.seq(x)
