b56fe53fb8810d556e63d7180f6ec29e9131ce1d
yukihito <ok220020@oks.iput.ac.jp>
2024-08-04 21:49:46 +0900

    修正

--- dev/frontend/reactapp/src/db/function/project_info.js
+++ dev/frontend/reactapp/src/db/function/project_info.js
@@ -3,7 +3,7 @@ import { db } from "../firebase";
 
 // 全ての画像分類プロジェクト情報を取得
 const getClassificationProjectInfo = async () => {
-  const docRef = doc(db, 'project_info', 'info');
+  const docRef = doc(db, 'project_info', 'image_classification_info');
   const docSnap = await getDoc(docRef);
   if (docSnap.exists()) {
     return docSnap.data()
@@ -14,7 +14,7 @@ const getClassificationProjectInfo = async () => {
 
 // 全ての強化学習プロジェクト情報を取得
 const getReinforcementlearningProjectInfo = async () => {
-  const docRef = doc(db, 'project_info', 'rl_info');
+  const docRef = doc(db, 'project_info', 'reinforcement_learning_info');
   const docSnap = await getDoc(docRef);
   if (docSnap.exists()) {
     return docSnap.data()
--- dev/frontend/reactapp/src/db/function/reader_bord.js
+++ dev/frontend/reactapp/src/db/function/reader_bord.js
@@ -3,12 +3,8 @@ import { db } from "../firebase";
 
 // リーダーボードの情報を取得
 const getReaderBoardInfo = async (projectId) => {
-  let projectName
-  if (projectId === 'CIFAR10') {
-    projectName = 'CIFAR-10_reader_board';
-  } else {
-    projectName = projectId + "_reader_board";
-  }
+  let projectName;
+  projectName = projectId + "_reader_board";
   const q = await query(collection(db, projectName), orderBy('accuracy', 'desc'));
   const querySnapshot = await getDocs(q);
   if (!querySnapshot.empty) {
