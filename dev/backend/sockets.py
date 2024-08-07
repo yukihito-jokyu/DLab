65458c2e59cd009ee9486401a43cb789e0060102
riku1021 <iriku63@gmail.com>
2024-08-08 03:58:16 +0900

    学習終了時にAccuracyとLossをmodel_managementに格納

--- dev/backend/sockets.py
+++ dev/backend/sockets.py
@@ -1,6 +1,6 @@
 from train.train_cartpole import train_cartpole
 from train.train_image_classification import train_model
-from utils.db_manage import update_status
+from utils.db_manage import update_status, save_result
 
 def setup_sockets(socketio):
     @socketio.on('connect')
@@ -22,5 +22,6 @@ def setup_sockets(socketio):
         model_id = datas['model_id']
         update_status(model_id, 'doing')
         accuracy, loss = train_model(datas)
+        save_result(model_id, accuracy, loss)
         update_status(model_id, 'done')
         socketio.emit('image_train_end'+model_id, {'message': 'train end'})
--- dev/backend/utils/db_manage.py
+++ dev/backend/utils/db_manage.py
@@ -13,3 +13,18 @@ def update_status(model_id, status):
         print(f'\nDocument with model_id {model_id} status updated to {status}.\n')
     else:
         print(f'\nDocument with model_id {model_id} does not exist.\n')
+
+def save_result(model_id, accuracy, loss):
+    db = get_firestore_db()
+    
+    # コレクションから該当するドキュメントを取得
+    doc_ref = db.collection('model_management').document(model_id)
+    
+    # ドキュメントの存在を確認してから更新
+    doc = doc_ref.get()
+    if doc.exists:
+        doc_ref.update({'accuracy': accuracy})
+        doc_ref.update({'loss': loss})
+        print(f'\nDocument with model_id {model_id} result updated to Acc:{accuracy} Loss:{loss}.\n')
+    else:
+        print(f'\nDocument with model_id {model_id} does not exist.\n')
