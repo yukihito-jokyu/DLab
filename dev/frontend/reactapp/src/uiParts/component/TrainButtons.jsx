1848ac73df8199225d5c8d1dd8d81bc72b1a64ad
riku1021 <iriku63@gmail.com>
2024-08-10 17:56:31 +0900

    学習中の時に学習ボタンを押せないように修正

--- dev/frontend/reactapp/src/uiParts/component/TrainButtons.jsx
+++ dev/frontend/reactapp/src/uiParts/component/TrainButtons.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useFetchStatus from '../../hooks/useFetchStatus';
 import '../css/TrainButtons.css';
 import { ReactComponent as TerminalIcon } from '../../assets/svg/terminal_24.svg'
 import { ReactComponent as PlayIcon } from '../../assets/svg/play_circle_24.svg'
 
 function TrainButtons({ changeEdit, changeTrain }) {
+  const { modelId } = useParams();
+  const currentStatus = useFetchStatus(modelId);
+
   return (
     <div className='train-buttons-wrapper'>
       <div className='terminal-wrapper' onClick={changeEdit} style={{ cursor: 'pointer' }}>
         <TerminalIcon className='terminal-svg' />
       </div>
-      <div className='play-wrapper' onClick={changeTrain} style={{ cursor: 'pointer' }}>
+      <div
+        className='play-wrapper'
+        onClick={currentStatus !== 'doing' ? changeTrain : null}
+        style={{
+          cursor: currentStatus !== 'doing' ? 'pointer' : 'not-allowed',
+          opacity: currentStatus !== 'doing' ? 1 : 0.7,
+        }}
+      >
         <PlayIcon className='play-svg' />
       </div>
     </div>
   )
 }
 
-export default TrainButtons
+export default TrainButtons;
