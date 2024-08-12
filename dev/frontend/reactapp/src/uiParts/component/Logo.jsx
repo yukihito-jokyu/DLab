324cc81d995631e0adcfa96fd3b81c7e5d176fa6
yukihito <ok220020@oks.iput.ac.jp>
2024-08-12 20:55:45 +0900

    ロゴを実装

--- dev/frontend/reactapp/src/uiParts/component/Logo.jsx
+++ dev/frontend/reactapp/src/uiParts/component/Logo.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import '../css/Logo.css'
+import React from 'react';
+import '../css/Logo.css';
+import { ReactComponent as LogoSVG } from '../../assets/svg/DLAB-logo.svg';
 
 function Logo() {
   return (
     <div className='logo-wrapper'>
-      <p>DLab</p>
+      {/* <p>DLab</p> */}
+      <LogoSVG className='logo-svg' />
     </div>
   )
 }
