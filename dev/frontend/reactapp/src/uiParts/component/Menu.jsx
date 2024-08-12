46ac81bd3bdaf7167fea26cccf1d5675508c6f78
riku1021 <iriku63@gmail.com>
2024-08-13 01:34:58 +0900

    サイドメニューのデザインを修正

--- dev/frontend/reactapp/src/uiParts/component/Menu.jsx
+++ dev/frontend/reactapp/src/uiParts/component/Menu.jsx
@@ -1,39 +1,63 @@
 import React from 'react';
 import '../css/Menu.css';
 import { useNavigate } from 'react-router-dom';
+import { ReactComponent as HomeIcon } from '../../assets/svg/home.svg';
+import { ReactComponent as ImageIcon } from '../../assets/svg/image.svg';
+import { ReactComponent as BirdIcon } from '../../assets/svg/bird.svg';
+import { ReactComponent as ShereIcon } from '../../assets/svg/shere.svg';
+import { ReactComponent as ProfileIcon } from '../../assets/svg/profile.svg';
 
 function Menu({ handleClickMenu }) {
   const userId = JSON.parse(sessionStorage.getItem('userId'));
   const navigate = useNavigate();
+
   const navTop = () => {
-    navigate('/top')
-  }
+    navigate('/top');
+  };
   const navImage = () => {
-    navigate(`/ImageClassificationProjectList/${userId}`)
-  }
+    navigate(`/ImageClassificationProjectList/${userId}`);
+  };
   const navReinforcement = () => {
-    navigate('/RLProjectList')
-  }
+    navigate('/RLProjectList');
+  };
   const navProjectShare = () => {
-    navigate('/projectshare')
-  }
+    navigate('/projectshare');
+  };
   const navProfile = () => {
-    navigate(`/profile/${userId}`)
-  }
+    navigate(`/profile/${userId}`);
+  };
+
   return (
     <div className='menu-wrapper'>
       <div className='menu-field-wrapper' onClick={handleClickMenu}></div>
       <div className='menu-field'>
-        <div className='menu-list-wrapper'>
-          <p onClick={navTop} style={{ cursor: 'pointer' }}>・Top</p>
-          <p onClick={navImage} style={{ cursor: 'pointer' }}>・Image Classification</p>
-          <p onClick={navReinforcement} style={{ cursor: 'pointer' }}>・Reinforcement Learning</p>
-          <p onClick={navProjectShare} style={{ cursor: 'pointer' }}>・Projects Share</p>
-          <p onClick={navProfile} style={{ cursor: 'pointer' }}>・Profile</p>
+        <div className='menu-field-inner'>
+          <div className='menu-list-wrapper'>
+            <div onClick={navTop} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+              <HomeIcon style={{ marginRight: '10px', width: '30px', height: '30px' }} />
+              <p style={{ margin: 15, lineHeight: '30px' }}>ホーム</p>
+            </div>
+            <div onClick={navImage} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+              <ImageIcon style={{ marginRight: '10px', width: '30px', height: '30px' }} />
+              <p style={{ margin: 15, lineHeight: '30px' }}>画像分類</p>
+            </div>
+            <div onClick={navReinforcement} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+              <BirdIcon style={{ marginRight: '10px', width: '30px', height: '30px' }} />
+              <p style={{ margin: 15, lineHeight: '30px' }}>強化学習</p>
+            </div>
+            <div onClick={navProjectShare} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+              <ShereIcon style={{ marginRight: '10px', width: '30px', height: '30px' }} />
+              <p style={{ margin: 15, lineHeight: '30px' }}>プロジェクト一覧</p>
+            </div>
+            <div onClick={navProfile} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+              <ProfileIcon style={{ marginRight: '10px', width: '30px', height: '30px' }} />
+              <p style={{ margin: 15, lineHeight: '30px' }}>プロフィール</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Menu;
