cf32fec42686cad470239beeff8a4233887eff7e
riku1021 <iriku63@gmail.com>
2024-08-24 01:44:53 +0900

    写真登録していなかったらデフォルトの画像を設置

--- dev/frontend/reactapp/src/uiParts/component/UserIcon.jsx
+++ dev/frontend/reactapp/src/uiParts/component/UserIcon.jsx
@@ -1,30 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import '../css/UserIcon.css'
+import '../css/UserIcon.css';
 import { useNavigate } from 'react-router-dom';
 import { getImage } from '../../db/function/storage';
+import DefoIcon from '../../assets/images/DLab_logo_400.png';
 
 function UserIcon() {
   const userId = JSON.parse(sessionStorage.getItem('userId'));
   const [userImage, setUserImage] = useState(null);
   const navigate = useNavigate();
-  // ユーザー画像
+
   useEffect(() => {
     const fetchUserImage = async () => {
       const url = await getImage(`images/${userId}`);
-      setUserImage(url)
-    }
+      setUserImage(url);
+    };
     fetchUserImage();
   }, [userId]);
+
   const handleNav = () => {
     navigate(`/profile/${userId}`);
-  }
+  };
+
   return (
     <div className='user-icon-wrapper' onClick={handleNav} style={{ cursor: 'pointer' }}>
-      <div className='user-icon'>
-        <img src={userImage} alt='icon' />
+      <div className={`user-icon ${!userImage ? 'default-icon-background' : ''}`}>
+        {userImage ? (
+          <img src={userImage} alt='icon' />
+        ) : (
+          <img src={DefoIcon} alt='default icon' />
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default UserIcon
+export default UserIcon;
