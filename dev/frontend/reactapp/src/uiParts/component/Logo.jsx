7dc611be282b2c08e1fa4f65ceab810bea5a41cc
riku1021 <iriku63@gmail.com>
2024-08-24 01:21:29 +0900

    ヘッダーロゴを押すとトップページに遷移するように修正

--- dev/frontend/reactapp/src/uiParts/component/Logo.jsx
+++ dev/frontend/reactapp/src/uiParts/component/Logo.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../css/Logo.css';
 import { ReactComponent as LogoSVG } from '../../assets/svg/DLAB-logo.svg';
 import { ReactComponent as LogoSVG1 } from '../../assets/svg/DLAB-logo_type1.svg';
 import { ReactComponent as LogoSVG5 } from '../../assets/svg/DLAB-logo_type5.svg';
 
 function Logo() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/top');
+  };
+
   return (
     <div className='logo-wrapper'>
       {/* <p>DLab</p> */}
       {/* <LogoSVG className='logo-svg' /> */}
-      <LogoSVG1 className='logo-svg' />
+      <LogoSVG1 className='logo-svg' onClick={handleClick} style={{ cursor: 'pointer' }} />
       {/* <LogoSVG5 className='logo-svg' /> */}
     </div>
-  )
+  );
 }
 
-export default Logo
+export default Logo;
